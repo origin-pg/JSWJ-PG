cd364806aff1d0378ce3a081ee1f959d5b0d3618
17610008766 <731590860@qq.com>
2019-04-22 16:05:14 +0800

    xx

--- JSWJ/assets/resources/tiledmap/wall.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="wall" tilewidth="32" tileheight="32" tilecount="378" columns="21">
- <image source="box.png" width="680" height="600"/>
-</tileset>
